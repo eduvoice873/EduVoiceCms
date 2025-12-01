36ab7aafbd04157374493530a79c7f76cea51eca
Lidia Alicia JG <lidiaaliciajg@gmail.com>
2025-12-01 14:47:38 -0600

    cambio ruta dashboard

--- app/(dashboard)/gestionTestimonio/page.tsx
+++ app/(dashboard)/gestionTestimonio/page.tsx
@@ -1,18 +1,14 @@
 "use client";
 import { TestimonialRender_admin } from "./testimonial_render-admin";
-import { testimonialsData } from "../landing/components/data";
-import DashboardLayout from "@/components/dashboard/DashboardLayout";
-
+import { testimonialsData } from "../../landing/components/data";
 
 export default function GestorTestimonial() {
     return (
-        <DashboardLayout>
             <div className="p-6">
                 <h1 className="text-xl font-semibold">Bienvenido al dashboard</h1>
                 <div className="mt-6">
                     <TestimonialRender_admin testimonials={testimonialsData} />
                 </div>
             </div>
-        </DashboardLayout>
     );
 }
